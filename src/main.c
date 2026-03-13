0f0150c2fba16dd0637c084ca59cf253746bb5d7
yben-dje <yben-dje@student.42angouleme.fr>
2026-03-13 21:30:43 +0100

    [Parser] Add a BIIIIIIIIIG tokeniser

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lchamard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 10:53:16 by lchamard          #+#    #+#             */
-/*   Updated: 2026/03/17 15:26:28 by lchamard         ###   ########.fr       */
+/*   Updated: 2026/03/17 17:10:55 by lchamard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,20 @@
 
 int	main(int argc, char **argv, char **env)
 {
-	(void)argc;
-	(void)env;
-	printf("%s\n", argv[0]);
+	t_vec parsed;
+	t_tokeniser_error result = tokenise(argv[1], &parsed);
+	if (result == tokeniser_error_succes)
+		printf("Parser success\n");
+	if (result == tokeniser_error_unterminated_quoted_string)
+		printf("Parser error: Unterminated quoted string.\n");
+
+	unsigned int i = 0;
+	while (i < parsed.size) {
+		t_vec cmd = *(t_vec *)vec_get(&parsed, i);
+		write(1, (char *)cmd.data, cmd.size);
+		write(1, "\n", 1);
+		vec_free(&cmd);
+		i++;
+	}
+	vec_free(&parsed);
 }
