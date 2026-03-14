5ac40b2de71d41884c17d40458f048c45392f4b9
Yannis <52321382+yannis300307@users.noreply.github.com>
2026-03-14 18:35:48 +0100

    [Parser] Add types for the tokens

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lchamard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 10:53:16 by lchamard          #+#    #+#             */
-/*   Updated: 2026/03/17 17:10:55 by lchamard         ###   ########.fr       */
+/*   Updated: 2026/03/17 17:12:49 by lchamard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,14 @@ int	main(int argc, char **argv, char **env)
 
 	unsigned int i = 0;
 	while (i < parsed.size) {
-		t_vec cmd = *(t_vec *)vec_get(&parsed, i);
-		write(1, (char *)cmd.data, cmd.size);
+		t_token cmd = *(t_token *)vec_get(&parsed, i);
+		if (cmd.type == token_type_plain)
+			write(1, "Plain:     ", 12);
+		else
+			write(1, "Delimiter: ", 12);
+		write(1, (char *)cmd.data.data, cmd.data.size);
 		write(1, "\n", 1);
-		vec_free(&cmd);
+		vec_free(&cmd.data);
 		i++;
 	}
 	vec_free(&parsed);
