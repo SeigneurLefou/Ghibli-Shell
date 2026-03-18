b9fc06dd5ce768d0a5501f28788acd16c791d7a3
Loic Chamard <lchamard@2B1.42angouleme.fr>
2026-03-18 11:36:27 +0100

    [vec to cmd] grab cmd before execute it

--- src/vec-to-cmd/vec_to_cmd.c
+++ src/vec-to-cmd/vec_to_cmd.c
@@ -6,15 +6,40 @@
 /*   By: lchamard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 09:00:48 by lchamard          #+#    #+#             */
-/*   Updated: 2026/03/18 09:55:08 by lchamard         ###   ########.fr       */
+/*   Updated: 2026/03/18 11:34:21 by lchamard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vec_to_cmd.h"
 
-int vec_to_cmd(t_btree *btree)
+int vec_to_cmd(t_btree_node *node, t_vec *expr)
 {
-	t_vec	data;
+	int	i;
+	t_cmd	new_node;
 
-	data = btree->expr;
+	i = node->expr_start;
+	while (i <= node->expr_end || ft_strcmp(vec_get(expr, i), "|"))
+	{
+		if (ft_strcmp(vec_get(expr, i), ">"))
+		{
+			i++;
+		}
+		else if (ft_strcmp(vec_get(expr, i), ">>"))
+		{
+			i++;
+		}
+		else if (ft_strcmp(vec_get(expr, i), "<"))
+		{
+			i++;
+		}
+		else if (ft_strcmp(vec_get(expr, i), "<<"))
+		{
+			i++;
+		}
+		else
+		{
+			i = i;
+		}
+		i++;
+	}
 }
