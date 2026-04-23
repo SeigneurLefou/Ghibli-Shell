513668c8c283dc2b441e7f49e850e38730ecc1d7
yben-dje <52321382+yannis300307@users.noreply.github.com>
2026-04-23 19:17:54 +0200

    [Shizuku] Cd without an argument cd to $HOME

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: yben-dje <yben-dje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 14:32:35 by yben-dje          #+#    #+#             */
-/*   Updated: 2026/04/20 19:21:21 by yben-dje         ###   ########.fr       */
+/*   Updated: 2026/04/23 19:15:54 by yben-dje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,33 @@
 int	builtin_cd(int argc, char **argv, t_minishell *minishell)
 {
 	char	path_buffer[PATH_MAX];
+	char	*home;
 
-	if (argc != 2)
+	if (argc > 2)
 	{
-		write(2, "CD needs one argument <path>.", 30);
+		write(2, "Too much arguments.\n", 21);
 		return (1);
 	}
-	if (!chdir(argv[1]))
+	if (argc == 1)
+	{
+		home = env_variable_manager_get_single(&minishell->env_variables_manager,
+				"HOME");
+		if (home)
+		{
+			if (!chdir(home))
+				perror("CD");
+		}
+	}
+	else
+	{
+		if (!chdir(argv[1]))
+			perror("CD");
+	}
+	if (!getcwd(path_buffer, PATH_MAX))
 		perror("CD");
-    if (!getcwd(path_buffer, PATH_MAX))
-		perror("CD");
-    env_variable_manager_set(&minishell->env_variables_manager, "PWD", path_buffer);
-    env_variable_manager_set(&minishell->env_variables_manager, "OLDPWD", path_buffer);
+	env_variable_manager_set(&minishell->env_variables_manager, "PWD",
+		path_buffer);
+	env_variable_manager_set(&minishell->env_variables_manager, "OLDPWD",
+		path_buffer);
 	return (0);
 }
