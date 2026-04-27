2360846a675d079ec93f4efc811775d4f4077875
Loic Chamard <lchamard@2A7.42angouleme.fr>
2026-04-27 06:44:17 +0200

    [Sheeta] Add the env command

--- includes/builtin.h
+++ includes/builtin.h
@@ -6,7 +6,11 @@
 /*   By: yben-dje <yben-dje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 14:43:54 by lchamard          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2026/04/27 06:39:31 by lchamard         ###   ########.fr       */
+=======
+/*   Updated: 2026/04/24 10:59:47 by yben-dje         ###   ########.fr       */
+>>>>>>> 8c84509 ([Sheeta] Add the env command)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +59,11 @@ int							builtin_source(int argc, char **argv,
 								t_minishell *minishell);
 int							builtin_unset(int argc, char **argv,
 								t_minishell *minishell);
+<<<<<<< HEAD
 int							builtin_exit(int argc,
 								t_minishell *minishell);
+=======
+int							builtin_env(int argc, t_minishell *minishell);
+>>>>>>> 8c84509 ([Sheeta] Add the env command)
 
 #endif
--- src/builtin/builtin_list.c
+++ src/builtin/builtin_list.c
@@ -6,7 +6,11 @@
 /*   By: yben-dje <yben-dje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 09:38:42 by lchamard          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2026/04/24 12:40:52 by yben-dje         ###   ########.fr       */
+=======
+/*   Updated: 2026/04/24 10:58:49 by yben-dje         ###   ########.fr       */
+>>>>>>> 8c84509 ([Sheeta] Add the env command)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +18,11 @@
 
 bool is_command_built_in(char *name)
 {
+<<<<<<< HEAD
 	const char *builtins[] = {"echo", "cd", "pwd", "export", "source", "unset", "exit", NULL};
+=======
+	const char *builtins[] = {"echo", "cd", "pwd", "export", "source", "unset", "env", NULL};
+>>>>>>> 8c84509 ([Sheeta] Add the env command)
 	unsigned int index;
 	
 	index = 0;
@@ -44,8 +52,13 @@ int	exec_builtin(t_cmd *cmds, t_minishell *minishell)
 		result = builtin_source(cmds->argc, cmds->argv, minishell);
 	if (!ft_strcmp(cmds->name, "unset"))
 		result = builtin_unset(cmds->argc, cmds->argv, minishell);
+<<<<<<< HEAD
 	if (!ft_strcmp(cmds->name, "exit"))
 		result = builtin_exit(cmds->argc, minishell);
+=======
+	if (!ft_strcmp(cmds->name, "env"))
+		result = builtin_env(cmds->argc, minishell);
+>>>>>>> 8c84509 ([Sheeta] Add the env command)
 	return (result);
 }
 
