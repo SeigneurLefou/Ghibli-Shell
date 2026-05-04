5a9342da2ced53f93fba1a1e796257aacd175fee
yben-dje <52321382+yannis300307@users.noreply.github.com>
2026-05-04 17:55:23 +0200

    [Haru] Probably fixed pipe priority issue

--- src/token/token_btree_parser.c
+++ src/token/token_btree_parser.c
@@ -59,11 +59,7 @@ int	get_next_delimiter(t_vec *expr, unsigned int index)
 	while (index > 0)
 	{
 		token = (t_token *)vec_get(expr, index);
-<<<<<<< HEAD
-		if (is_a_delimiter(token, true, true))
-=======
 		if (is_a_delimiter(token, true, false))
->>>>>>> ad47537 ([Haru] Probably fixed pipe priority issue)
 			break ;
 		index --;
 	}
@@ -87,7 +83,11 @@ bool	is_in_parentheses(t_vec *expr, unsigned int index, unsigned int end)
 		if (is_a_delimiter(token, false, true))
 =======
 		if (is_a_delimiter(token, false, false))
+<<<<<<< HEAD
 >>>>>>> ad47537 ([Haru] Probably fixed pipe priority issue)
+=======
+>>>>>>> ee09741 ([Haru] Probably fixed pipe priority issue)
+>>>>>>> d8effb4 ([Haru] Probably fixed pipe priority issue)
 			return (false);
 		if (token->type == token_type_scope_delimiter && token->data.data[0] == ')')
 			break ;
