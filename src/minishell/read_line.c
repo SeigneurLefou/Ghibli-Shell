aee609c81b2e5497f2ff4a680d2f4a9753f2fe17
Loic Chamard <lchamard@2B3.42angouleme.fr>
2026-05-05 18:28:56 +0200

    [leaks] stqart to fix the leaks in the file reader

--- src/minishell/read_line.c
+++ src/minishell/read_line.c
@@ -80,6 +80,54 @@ void	handle_prompt(t_minishell *minishell)
 		if (minishell->request_exit)
 			break ;
 	}
+	free(line);
+	free(trimmed);
 	close(stdin_save);
 	rl_clear_history();
 }
+/*
+while (1)
+	{
+		prompt_line = get_prompt_line(minishell);
+		line = NULL;
+		if (prompt_line)
+		{
+			line = readline(prompt_line);
+			free(prompt_line);
+		}
+		else
+			line = readline("$>");
+		if (!line)
+		{
+			if (g_signal > 0)
+			{
+				g_signal = -1;
+				dup2(stdin_save, 0);
+				rl_replace_line("", 1);
+				if (first_sigint)
+					write(1, "\n", 1);
+				rl_on_new_line();
+				first_sigint = false;
+				continue;
+			}
+			else
+				minishell->request_exit = true;
+		}
+		else
+			trimmed = ft_strtrim(line, "\r\n \t");
+		if (line && trimmed)
+		{
+			if (trimmed[0])
+			{
+				first_sigint = true;
+				add_history(trimmed);
+				add_to_history_file(minishell, ".ghiblistory", trimmed);
+				main_token(trimmed, minishell);
+				free(trimmed);
+			}
+		}
+		free(line);
+		if (minishell->request_exit)
+			break;
+	}
+	*/
