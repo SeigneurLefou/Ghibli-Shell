0fda3903e4219032cf2cadab82b5078db4551b34
SeigneurLefou <lchamard@proton.me>
2026-05-12 16:45:37 +0200

    [sheeta miyazaki] fix the conflicts and remove debug

--- src/btree/exec_binary_tree.c
+++ src/btree/exec_binary_tree.c
@@ -82,8 +82,6 @@ bool	exec_leaf(t_btree *tree, int files[2], t_vec *pid_list)
 	char	*status;
 
 	vec_to_cmd(tree);
-	printf("cmd : %s\n", tree->node->cmds->name);
-	write(1, "aaaaaaaaa\n", 10);
 	if (tree->node->cmds->name && is_command_built_in(tree->node->cmds->name))
 		setup_and_exec_builtin(tree, files);
 	else
