12efae9c14ff8d908702e581846d2263af562b88
yben-dje <52321382+yannis300307@users.noreply.github.com>
2026-05-13 18:51:50 +0200

    [Ponyo] Add a function to query files in a directory

--- src/builtin/expand.c
+++ src/builtin/expand.c
@@ -6,7 +6,7 @@
 /*   By: yben-dje <yben-dje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 06:45:58 by lchamard          #+#    #+#             */
-/*   Updated: 2026/05/13 13:11:15 by yben-dje         ###   ########.fr       */
+/*   Updated: 2026/05/13 18:51:15 by yben-dje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,6 +100,118 @@ bool	expand_split(t_vec *argv, t_vec *new_line, char *var_content)
 	}
 	vec_free(&var_split);
 	return (!var_split.failed);
+}
+
+char *path_join(char *a, char *b)
+{
+	unsigned int a_size;
+	char *path;
+
+	a_size = ft_strlen(a);
+	if (a[a_size - 1] == '/')
+		path = ft_strjoin(a, b);
+	else
+	{
+		path = ft_strjoin(a, "/");
+		if (!path)
+			return (false);
+		path = ft_strjoin(path, b);
+	}
+	return (path);
+}
+
+struct s_parsed_file_name
+{
+	char *file_name;
+	char *extension;
+	bool match_all;
+};
+
+struct s_parsed_file_name parse_file_name(char *name)
+{
+	char *dot;
+	struct s_parsed_file_name parsed;
+	
+	ft_bzero(&parsed, sizeof(struct s_parsed_file_name));
+	parsed.match_all = !ft_strcmp(name, "*");
+	if (parsed.match_all)
+		return (parsed);
+	dot = ft_strchr(name, '.');
+	if (!dot)
+	{
+		parsed.file_name = ft_strdup(name);
+		return (parsed);
+	}
+	parsed.extension = ft_strdup(dot + 1);
+	if (!parsed.extension)
+		return (parsed);
+	parsed.file_name = malloc(dot - name + 1);
+	if (!parsed.file_name)
+	{
+		free(parsed.extension);
+		parsed.extension = NULL;
+		return (parsed);
+	}
+	parsed.file_name[dot - name] = 0;
+	ft_memcpy(parsed.file_name, name, dot - name);
+	return (parsed);
+}
+
+
+void free_parsed_file_name(struct s_parsed_file_name parsed)
+{
+	free(parsed.extension);
+	free(parsed.file_name);
+}
+
+bool	file_matches_filter(struct s_parsed_file_name filter, char *name)
+{
+	struct s_parsed_file_name parsed;
+	bool file_name_match;
+	bool extension_match;
+	
+	file_name_match = false;
+	extension_match = false;
+	if (filter.match_all)
+		return (true);
+	parsed = parse_file_name(name);
+	if (parsed.file_name == filter.file_name || (!ft_strcmp(filter.file_name, "*") || !ft_strcmp(filter.file_name, parsed.file_name)))
+		file_name_match = true;
+	if (parsed.extension == filter.extension || (!ft_strcmp(filter.extension, "*") || !ft_strcmp(filter.extension, parsed.extension)))
+		extension_match = true;
+	free_parsed_file_name(parsed);
+	return (file_name_match && extension_match);
+}
+
+#include <dirent.h>
+bool	query_files_in_dir(t_vec *files, char *path, char *filter)
+{
+	DIR *dir;
+	struct dirent *dir_entry;
+	char *file_path;
+	struct s_parsed_file_name parsed_filter;
+
+	parsed_filter = parse_file_name(filter);
+	dir = opendir(path);
+	if (!dir)
+		return (false);
+	dir_entry = readdir(dir);
+	while (dir_entry)
+	{
+		if (file_matches_filter(parsed_filter, dir_entry->d_name))
+		{
+			file_path = path_join(path, dir_entry->d_name);
+			if (!file_path)
+				{
+					return (false);
+					closedir(dir);
+				}
+			vec_append(files, file_path);
+		}
+		dir_entry = readdir(dir);
+	}
+	closedir(dir);
+	return (!files->failed);
 }
 
 bool	expand(t_vec *argv, t_token *token, t_minishell *minishell)
