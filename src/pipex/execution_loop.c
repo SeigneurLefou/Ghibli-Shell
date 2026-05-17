d16e892249dd479d53566509f00cf7fa3a80075d
Yannis <52321382+yannis300307@users.noreply.github.com>
2026-05-17 19:40:49 +0200

    [Umi] Remade signal handling from scratch

--- src/pipex/execution_loop.c
+++ src/pipex/execution_loop.c
@@ -6,7 +6,7 @@
 /*   By: yben-dje <yben-dje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 17:57:48 by lchamard          #+#    #+#             */
-/*   Updated: 2026/05/17 17:05:00 by yben-dje         ###   ########.fr       */
+/*   Updated: 2026/05/17 19:06:43 by yben-dje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	fork_pid(t_pipex *pipex_var, int stdin_pid_copy)
 		return (1);
 	else if (pipex_var->pid == 0)
 	{
+		signal(SIGINT, SIG_DFL);
+		signal(SIGTERM, SIG_DFL);
 		close(stdin_pid_copy);
 		take_child(pipex_var);
 	}
