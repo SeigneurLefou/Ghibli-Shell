4c68bd813c096abf8f43de89a9bf272f7fdb095a
Loic Chamard <lchamard@2B1.42angouleme.fr>
2026-05-21 18:10:29 +0200

    [miyazaki] fix the expand when there is space

---
+++ abc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	while (argv[i])
+	{
+		printf("argv[%d] -> {%s}\n", i, argv[i]);
+		i++;
+	}
+}
--- src/expands/expands.c
+++ src/expands/expands.c
@@ -6,7 +6,7 @@
 /*   By: yben-dje <yben-dje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 06:45:58 by lchamard          #+#    #+#             */
-/*   Updated: 2026/05/18 14:10:46 by yben-dje         ###   ########.fr       */
+/*   Updated: 2026/05/21 18:06:15 by lchamard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,18 +81,23 @@ bool	expand_split(t_vec *argv, t_vec *expanded_token, char *var_content)
 	t_vec	var_split;
 	size_t	j;
 
+	j = 1;
 	if (!var_content)
 		return (true);
 	vec_init(&var_split, sizeof(t_vec), 5);
 	vec_split(&var_split, var_content, ' ');
 	if (!var_split.size)
 		return (false);
-	vec_expand(expanded_token, vec_get(&var_split, 0));
+	if (expanded_token->size > 0
+			&& *(char *)vec_get(expanded_token, expanded_token->size - 1) != ' '
+			&&  var_content && var_content[0] && var_content[0] != ' ')
+		vec_expand(expanded_token, vec_get(&var_split, 0));
+	else
+		j = 0;
 	if (var_split.size > 1)
 	{
 		vec_append(argv, expanded_token);
 		vec_init(expanded_token, sizeof(char), 20);
-		j = 1;
 		while (j < var_split.size - 1)
 		{
 			vec_append(argv, vec_get(&var_split, j));
