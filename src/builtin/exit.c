4cafb8979d3055e70783eafa3756bf6ccedcd5c7
Yannis <52321382+yannis300307@users.noreply.github.com>
2026-05-26 17:57:50 +0200

    [Sheeta] Fix exit code modulo

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: yben-dje <yben-dje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 14:32:35 by yben-dje          #+#    #+#             */
-/*   Updated: 2026/05/26 17:32:20 by yben-dje         ###   ########.fr       */
+/*   Updated: 2026/05/26 17:36:22 by yben-dje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	builtin_exit(int argc, char **argv, t_minishell *minishell)
 		return (1);
 	}
 	if (argc == 2)
-		minishell->last_status = ft_atol(argv[1]) % 255;
+		minishell->last_status = ft_atol(argv[1]) & 0xFF;
 	minishell->request_exit = true;
 	return (minishell->last_status);
 }
