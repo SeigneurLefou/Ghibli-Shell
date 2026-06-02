843b740d2b26fe4c2afb3cb0409412528c0ce5f6
yben-dje <52321382+yannis300307@users.noreply.github.com>
2026-06-02 14:07:43 +0200

    [Sheeta] Add checks for invalid numerical argument

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,15 +6,33 @@
 /*   By: yben-dje <yben-dje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 14:32:35 by yben-dje          #+#    #+#             */
-/*   Updated: 2026/06/01 18:38:01 by yben-dje         ###   ########.fr       */
+/*   Updated: 2026/06/02 14:07:28 by yben-dje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
+bool	is_numeric(char *num)
+{
+	unsigned int	index;
+
+	index = 0;
+	if (num[0] == '+' || num[0] == '-')
+		index++;
+	if (!num[index])
+		return (false);
+	while (num[index])
+	{
+		if (!ft_isdigit(num[index]))
+			return (false);
+		index++;
+	}
+	return (true);
+}
+
 int	builtin_exit(int argc, char **argv, t_minishell *minishell)
 {
-	bool overflow;
+	bool	overflow;
 
 	overflow = false;
 	if (argc > 2)
@@ -23,8 +41,14 @@ int	builtin_exit(int argc, char **argv, t_minishell *minishell)
 		return (1);
 	}
 	if (argc == 2)
-		minishell->last_status = ft_atol_check_overflow(argv[1], &overflow) & 0xFF;
+		minishell->last_status = ft_atol_check_overflow(argv[1],
+				&overflow) & 0xFF;
 	minishell->request_exit = true;
+	if (!is_numeric(argv[1]))
+	{
+		write(2, "The argument is not a valid number. But bye!\n", 46);
+		return (2);
+	}
 	if (overflow)
 	{
 		write(2, "Exit code is larger than long max. But bye!\n", 45);
