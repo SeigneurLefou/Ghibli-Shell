6123b005c24fb8628c42ac053f943d8f9c938d35
yben-dje <52321382+yannis300307@users.noreply.github.com>
2026-06-02 14:54:04 +0200

    [Sheeta] Fix segfault in exit

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: yben-dje <yben-dje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 14:32:35 by yben-dje          #+#    #+#             */
-/*   Updated: 2026/06/02 14:07:28 by yben-dje         ###   ########.fr       */
+/*   Updated: 2026/06/02 14:44:59 by yben-dje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,12 +44,12 @@ int	builtin_exit(int argc, char **argv, t_minishell *minishell)
 		minishell->last_status = ft_atol_check_overflow(argv[1],
 				&overflow) & 0xFF;
 	minishell->request_exit = true;
-	if (!is_numeric(argv[1]))
+	if (argc == 2 && !is_numeric(argv[1]))
 	{
 		write(2, "The argument is not a valid number. But bye!\n", 46);
 		return (2);
 	}
-	if (overflow)
+	if (argc == 2 && overflow)
 	{
 		write(2, "Exit code is larger than long max. But bye!\n", 45);
 		return (2);
