8e06380eba2720bd6efd8681069b2ef55798315a
yben-dje <52321382+yannis300307@users.noreply.github.com>
2026-06-05 20:26:10 +0200

    [Refactor] Norme random headers NORME IS FINISHED !!!!!!!

--- includes/environment_variables.h
+++ includes/environment_variables.h
@@ -6,7 +6,7 @@
 /*   By: yben-dje <yben-dje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 13:52:38 by yben-dje          #+#    #+#             */
-/*   Updated: 2026/06/01 19:51:39 by yben-dje         ###   ########.fr       */
+/*   Updated: 2026/06/05 20:25:28 by yben-dje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,33 @@ typedef struct s_env_variables_manager
 }						t_env_variables_manager;
 
 t_env_variables_manager	env_variables_new(void);
-bool					env_variables_add(t_env_variables_manager *env_variable_manager,
+bool					env_variables_add(
+							t_env_variables_manager *env_variable_manager,
 							char *key, char *value);
-char					**env_variables_get_env(t_env_variables_manager *env_variable_manager);
-char					*env_variables_get(t_env_variables_manager *env_variable_manager,
+char					**env_variables_get_env(
+							t_env_variables_manager *env_variable_manager);
+char					*env_variables_get(
+							t_env_variables_manager *env_variable_manager,
 							char *key);
-bool					env_variables_set(t_env_variables_manager *env_variable_manager,
+bool					env_variables_set(
+							t_env_variables_manager *env_variable_manager,
 							char *key, char *value);
-bool					env_variables_unset_key(t_env_variables_manager *env_variable_manager,
+bool					env_variables_unset_key(
+							t_env_variables_manager *env_variable_manager,
 							char *key);
-void					env_variables_free(t_env_variables_manager *env_variable_manager);
-void					env_variables_add_from_env(t_env_variables_manager *env_variable_manager,
+void					env_variables_free(
+							t_env_variables_manager *env_variable_manager);
+void					env_variables_add_from_env(
+							t_env_variables_manager *env_variable_manager,
 							char **env);
-bool					env_variables_exists(t_env_variables_manager *env_variable_manager,
+bool					env_variables_exists(
+							t_env_variables_manager *env_variable_manager,
 							char *key);
-bool					env_variables_set_raw(t_env_variables_manager *env_variable_manager,
+bool					env_variables_set_raw(
+							t_env_variables_manager *env_variable_manager,
 							char *line);
-char					*env_variables_get_raw(t_env_variables_manager *env_variable_manager,
+char					*env_variables_get_raw(
+							t_env_variables_manager *env_variable_manager,
 							char *key);
 char					*env_variables_manager_create_line(char *key,
 							char *value);
