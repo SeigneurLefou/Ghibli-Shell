dcb3720338e6005d998e12caa4245fc9d553ac3a
yben-dje <52321382+yannis300307@users.noreply.github.com>
2026-06-05 16:13:17 +0200

    [Umi] Don't Close stdin when the signals are disabled

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: yben-dje <yben-dje@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 15:30:28 by yben-dje          #+#    #+#             */
-/*   Updated: 2026/06/05 15:48:36 by yben-dje         ###   ########.fr       */
+/*   Updated: 2026/06/05 16:12:06 by yben-dje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,4 @@ void	handle_signal_disabled(int sig)
 		write(2, "Quit\n", 6);
 	else
 		write(2, "\n", 1);
-	close(0);
 }
